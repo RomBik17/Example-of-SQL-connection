29624631ccf3d5eae191a68deb9e97a6291dec99
RomBik17 <54263329+RomBik17@users.noreply.github.com>
2020-03-31 22:28:25 +0300

    Initial commit

---
+++ WindowsFormsApp/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace WindowsFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dGV1 = new System.Windows.Forms.DataGridView();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dGV1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dGV1
+            // 
+            this.dGV1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dGV1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dGV1.Location = new System.Drawing.Point(12, 12);
+            this.dGV1.Name = "dGV1";
+            this.dGV1.RowHeadersWidth = 51;
+            this.dGV1.RowTemplate.Height = 24;
+            this.dGV1.Size = new System.Drawing.Size(714, 207);
+            this.dGV1.TabIndex = 0;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 236);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(60, 21);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Date";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(12, 263);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(111, 21);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "Organization";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(12, 290);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(53, 21);
+            this.checkBox3.TabIndex = 3;
+            this.checkBox3.Text = "City";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(12, 318);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(79, 21);
+            this.checkBox4.TabIndex = 4;
+            this.checkBox4.Text = "Country";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(12, 346);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(94, 21);
+            this.checkBox5.TabIndex = 5;
+            this.checkBox5.Text = "Mananger";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            this.checkBox5.CheckedChanged += new System.EventHandler(this.checkBox5_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(285, 290);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 68);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Sort";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(751, 525);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.dGV1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dGV1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dGV1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApp/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        string connectionString;
+        SqlConnection sqlConnection;
+        bool checkData = false,
+             checkOrg = false,
+             checkCity = false,
+             checkCountry = false,
+             checkMan = false;
+        string stringDate = "",
+               stringOrg = "",
+               stringCity = "",
+               stringCountry = "",
+               stringMan = "";
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            sqlConnection.Close();
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            checkOrg = !checkOrg;
+            if (checkBox2.Checked) stringOrg = "[Organization], ";
+            else stringOrg = "";
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            checkCountry = !checkCountry;
+            if (checkBox4.Checked) stringCountry = "[Country], ";
+            else stringCountry = "";
+        }
+
+        private void checkBox5_CheckedChanged(object sender, EventArgs e)
+        {
+            checkMan = !checkMan;
+            if (checkBox5.Checked) stringMan = "[Mananger], ";
+            else stringMan = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dGV1.DataSource = null;
+            connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\rrafa\Desktop\WindowsFormsApp\WindowsFormsApp\Database1.mdf;Integrated Security=True";
+            sqlConnection = new SqlConnection(connectionString);
+            if (!(checkMan || checkOrg || checkCity || checkCountry || checkData))
+            {
+                using (sqlConnection)
+                {
+                    sqlConnection.Open();
+                    SqlDataAdapter sqlDataAdapter = new SqlDataAdapter("SELECT * FROM [base_Table]", sqlConnection);
+                    DataTable dtb = new DataTable();
+                    sqlDataAdapter.Fill(dtb);
+                    dGV1.DataSource = dtb;
+                }
+            }
+            else
+            {
+                string select = stringDate + stringOrg + stringCity + stringCountry + stringMan;
+                int x = select.Length - 2;
+                string group = select.Remove(x);
+                using (sqlConnection)
+                {
+                    sqlConnection.Open();
+                    SqlDataAdapter sqlDataAdapter = new SqlDataAdapter("SELECT "+select+"SUM([Count]) AS [Count], SUM([Summ]) AS [Summ] FROM [base_Table] GROUP BY "+group, sqlConnection);
+                    DataTable dtb = new DataTable();
+                    sqlDataAdapter.Fill(dtb);
+                    dGV1.DataSource = dtb;
+                }
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            checkData = !checkData;
+            if (checkBox1.Checked) stringDate = "[Date], ";
+            else stringDate = "";
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            checkCity = !checkCity;
+            if (checkBox3.Checked) stringCity = "[City], ";
+            else stringCity = "";
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\rrafa\Desktop\WindowsFormsApp\WindowsFormsApp\Database1.mdf;Integrated Security=True";
+            sqlConnection = new SqlConnection(connectionString);
+            using (sqlConnection)
+            {
+                sqlConnection.Open();
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter("SELECT * FROM [base_Table]", sqlConnection);
+                DataTable dtb = new DataTable();
+                sqlDataAdapter.Fill(dtb);
+                dGV1.DataSource = dtb;
+            }
+        }
+    }
+}
